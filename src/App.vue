d8b88f450f4191ece293fff20b7209f5024b8647
Mr.beam <70924959+mrbeamuse@users.noreply.github.com>
2023-08-13 23:05:45 +0800

    添加竖向柱状图（服务资源占用比）

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div
-    class="bg-[url('@/assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-5 flex overflow-hidden"
+    class="bg-[url('@/assets/imgs/bg.jpg')] bg-cover bg-center h-screen text-white p-2 flex overflow-hidden"
     v-if="data"
   >
-    <div class="flex-1 mr-5 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
+    <div class="flex-1 mr-2 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
       <!-- 横向柱状图 -->
-      <HorizontalBar :data="data.regionData" class="h-1/3 box-border pb-4" />
+      <HorizontalBar class="h-1/3 box-border pb-4" :data="data.regionData" />
       <!-- 雷达图 -->
-      <RadarBar class="h-1/3 box-border pb-4" />
+      <RadarBar class="h-1/3 box-border pb-4" :data="data.riskData" />
       <!-- 数据传递关系图 -->
-      <Relation class="h-1/3" />
+      <Relation class="h-1/3" :data="data.relationData" />
     </div>
-    <div class="w-1/2 mr-5 flex flex-col">
+    <div class="w-1/2 mr-2 flex flex-col">
       <!-- 数据展示图 -->
-      <TotalData class="bg-opacity-50 bg-slate-800 p-3" />
+      <TotalData class="bg-opacity-50 bg-slate-800 p-3" :data="data.totalData" />
       <!-- 地图可视化 -->
-      <MapChart class="bg-opacity-50 bg-slate-800 p-3 mt-4 flex-1" />
+      <MapChart class="bg-opacity-50 bg-slate-800 p-3 mt-2 flex-1" :data="data.mapData" />
     </div>
     <div class="flex-1 bg-opacity-50 bg-slate-800 p-3 flex flex-col">
       <!-- 竖向柱状图 -->
-      <VerticalBar class="h-1/3 box-border pb-4" />
+      <VerticalBar class="h-1/3 box-border pb-4" :data="data.serverData" />
       <!-- 环形资源站比图 -->
-      <RadiueBar class="h-1/3 box-border pb-4" />
+      <RadiueBar class="h-1/3 box-border pb-4" :data="data.abnormalData" />
       <!-- 文档云图 -->
-      <WordCloud class="h-1/3 box-border" />
+      <WordCloud class="h-1/3 box-border" :data="data.wordCloudData" />
     </div>
   </div>
 </template>
